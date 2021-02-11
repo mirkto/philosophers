83fe341543bafda87fb01b01bab7f4fcf33b25f2
mirkto <thehisev@mail.ru>
2021-02-11 17:55:31 +0300

    monie

--- philo_one.c
+++ philo_one.c
@@ -28,8 +28,9 @@ typedef struct	s_param
 	long			number_of_times_each_philosopher_must_eat;
 	long			time_start;
 	int				num;
-	int				*philo_life_status;
+	long			*philo_life_status;
 	pthread_t		p_exit;
+	pthread_t		p_time;
 	int				exit_status;
 	pthread_mutex_t	mutex_es;
 	pthread_mutex_t	*mutex;
@@ -59,21 +60,7 @@ long	time_now()
 
 long	time_passed(long start_time)
 {
-	long	time_end;
-
-	time_end = time_now();
-	return (time_end - start_time);
-}
-
-long	sleep_and_time(t_param *all, long time)
-{
-	long	now;
-
-	usleep(time);
-	now = time_passed(all->time_start);
-	// printf("|%li|\n", now);
-	// printf("|%li|\n", now % 10);
-	return (now);
+	return (time_now() - start_time);
 }
 
 void	check_exit_status_and_print(t_param *all, int num, long time, char *str)
@@ -84,16 +71,30 @@ void	check_exit_status_and_print(t_param *all, int num, long time, char *str)
 	pthread_mutex_unlock(&all->mutex_es);
 }
 
+void	monie(long timer)
+{
+	long start_time;
+
+	start_time = time_now();
+	usleep(timer - 7000);
+	printf("%ld\n", time_now() - start_time);
+	while(time_now() - start_time < timer)
+	{
+		usleep(100);
+	}
+	printf("pozral\n");
+}
+
 void	*philo(void *tmp)
 {
 	t_param *all = tmp;
 	int		philo_num;
 	long	time;
-	long	time_after_eat;
+	long	time_eat;
 	pthread_mutex_t	*mutex_left_fork;
 	pthread_mutex_t	*mutex_right_fork;
 
-	time_after_eat = -1;
+	time_eat = -1;
 	philo_num = all->num++;
 	// printf("Philosopher %i is ready\n", philo_num);//---
 	mutex_left_fork = &all->mutex[philo_num - 1];
@@ -101,27 +102,31 @@ void	*philo(void *tmp)
 	while (all->exit_status == 0)
 	{
 		pthread_mutex_lock(mutex_left_fork);
-		time = sleep_and_time(all, 0);
+		time = time_passed(all->time_start);
 		check_exit_status_and_print(all, philo_num, time, "taken a fork\n");
 		pthread_mutex_lock(mutex_right_fork);
-		time = sleep_and_time(all, 0);
+		time = time_passed(all->time_start);
 		check_exit_status_and_print(all, philo_num, time, "taken a fork\n");
 
-		time = sleep_and_time(all, all->time_to_eat);
+		time = time_passed(all->time_start);
+		all->philo_life_status[philo_num - 1] = time_now();
 		check_exit_status_and_print(all, philo_num, time, "is eating\n");
+		usleep(all->time_to_eat);
+		// monie(all->time_to_eat);
 		pthread_mutex_unlock(mutex_left_fork);
 		pthread_mutex_unlock(mutex_right_fork);
 
-		time = sleep_and_time(all, all->time_to_sleep);
+		time = time_passed(all->time_start);
 		check_exit_status_and_print(all, philo_num, time, "is sleeping\n");
+		usleep(all->time_to_sleep);
 
-		time = sleep_and_time(all, 0);
+		time = time_passed(all->time_start);
 		check_exit_status_and_print(all, philo_num, time, "is thinking\n");
 
-		while (1)
-		{
-			all->time_to_die
-		}
+		// while (1)
+		// {
+		// 	all->time_to_die;
+		// }
 
 		pthread_mutex_lock(&all->mutex_es);
 		if (all->exit_status == 0)
@@ -195,13 +200,12 @@ void	inits(t_param *all)
 {
 	int			i;
 
-	all->philo_life_status = (int *)malloc(sizeof(int) * all->number_of_philosophers);
+	all->philo_life_status = (long *)malloc(sizeof(long) * all->number_of_philosophers);
 	i = -1;
 	while (++i < all->number_of_philosophers)
 		all->philo_life_status[i] = 0; 
 	pthread_mutex_init(&all->mutex_es, NULL);
 	all->num = 1;
-	all->time_start = time_now();
 	all->exit_status = 0;
 }
 
@@ -222,7 +226,7 @@ int	init_philo(t_param *all)
 			printf("---|Alart|--- %d philo is not born! ---|Alart|---", i + 1);
 			return (1);
 		}
-		usleep(50);//--time after born philo
+		// usleep(50);//--time after born philo
 		i++;
 	}
 	return (0);
@@ -241,6 +245,16 @@ void	init_fork(t_param *all)
 	}
 }
 
+// void	*p_time(void *tmp)
+// {
+// 	t_param	*all = tmp;
+// 	int		i;
+// 	long	time;
+
+// 	time = 0;
+// 	while ()
+// }
+
 void	*p_exit(void *tmp)
 {
 	t_param	*all = tmp;
@@ -250,12 +264,15 @@ void	*p_exit(void *tmp)
 	while (1)
 	{
 		// time = sleep_and_time(all, 10000);
-		usleep(5000);
+		// usleep(5000);
 		time = time_passed(all->time_start);
 		i = 0;
 		while (i < all->number_of_philosophers)
 		{
-			if (all->philo_life_status[i] == 1)
+			// printf("---| %li %li |---\n", (time_now() - all->philo_life_status[i]), all->time_to_die);
+
+			// if ((time_now() - all->philo_life_status[i]) > all->time_to_die && all->philo_life_status[i] != 0)
+			if (all->philo_life_status[i] == 0)
 			{
 				printf("%li %i is died\n", time, i + 1);
 				write(1, "Exit\n", 5);
@@ -278,6 +295,9 @@ int		main(int argc, char **argv)
 	// print_param(&all);//--print from all
 	inits(&all);
 	init_fork(&all);
+	all.time_start = time_now();
+		// pthread_create(&all.p_time, NULL, p_time, (void *)&all);
+		// pthread_join(all.p_exit, NULL);
 	init_philo(&all);
 	//-----wait_philo----------
 	// i = -1;
